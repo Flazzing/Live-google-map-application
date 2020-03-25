0358fa10cd377430a66dfa24d8753769d288a26d
flazzing <limchengqing@gmail.com>
2020-03-25 07:15:27 -0700

    fixed the filing pathing

--- live-map/src/App.js
+++ live-map/src/App.js
@@ -4,14 +4,7 @@ import "./App.css";
 import LiveMap from "./LiveMap";
 
 const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h3>This is the map of the location.</h3>
-				<LiveMap />
-			</header>
-		</div>
-	);
+	return <LiveMap />;
 };
 
 export default App;
--- live-map/src/index.js
+++ live-map/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./LiveMap";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
